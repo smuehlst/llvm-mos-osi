be093c3ec0548ca962e4b474f2d5baa4ec3e80bb
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2022-04-18 18:48:33 +0200

    First minimal OSI C1P program with llvm-mos-sdk
    Writes a string directly into video memory.

---
+++ minimal/minimal.c
@@ -0,0 +1,19 @@
+/*
+ * Challenger 1P llvm-mos minimal test program.
+ *
+ * It writes a hello world text directly to video RAM.
+ */
+#include <string.h>
+
+int
+main(void)
+{
+    static const char str[] = "Hello world with llvm-mos!";
+    char * const video_ram_start = (char *) 0xd000;
+
+    memcpy(video_ram_start + 0x224, str, sizeof(str) - 1);
+
+    while (1);
+	
+    return 0;
+}
