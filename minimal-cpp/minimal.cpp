cbdfcd45d3169571a1ff38aed8bef33983dda94a
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2022-04-20 18:58:22 +0200

    Minimal examples for llvm-mos-sdk OSI

---
+++ minimal-cpp/minimal.cpp
@@ -0,0 +1,24 @@
+/*
+ * Challenger 1P llvm-mos minimal test program.
+ *
+ * It writes a hello world text directly to video RAM.
+ */
+#include <string.h>
+
+int
+main(void)
+{
+    static const char str[] = "Hello world with llvm-mos C++!";
+    char * const video_ram_start = (char *) 0xd000;
+
+    char *vp = video_ram_start + 0x224;
+    for (auto const c: str)
+    {
+        if (c)
+        {
+            *vp++ = c;
+        }
+    }
+
+    while (1);
+}
